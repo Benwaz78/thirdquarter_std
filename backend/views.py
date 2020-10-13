442f18a7fa58581f44ced640229136d99e915980
Benwaz78 <nonwaz78@gmail.com>
2020-10-14 00:05:21 +0100

    My message

--- backend/views.py
+++ backend/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def login(request):
     return HttpResponse('<h1>Login Page</h1>')
 
+
 def register(request):
     return HttpResponse('<h1>Register Page</h1>')
+
+
+def dashboard(request):
+    return render(request, 'backend/index.html')
