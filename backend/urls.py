e316b8b70d2670c88bdec5c6c85bd99cd9973cf9
Benwaz78 <nonwaz78@gmail.com>
2020-10-14 00:52:24 +0100

    My changes

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,7 @@ from backend import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('backoffice/', views.dashboard, name='dashboard'),
     path('register-page/', views.register, name='register')
 ]
 
